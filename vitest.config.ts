0b65bd570ddfe9d8ecb9e0e7cbbed4bb99bd0788
Steve Jung <steve@lemoncloud.io>
2026-03-26 15:34:02 +0900

    refact: opt eslint

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,11 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'node',
-    include: ['src/**/*.spec.ts'],
-    coverage: {
-      reporter: ['text', 'html']
-    }
-  }
+    test: {
+        environment: 'node',
+        include: ['src/**/*.spec.ts'],
+        coverage: {
+            reporter: ['text', 'html'],
+        },
+    },
 });
